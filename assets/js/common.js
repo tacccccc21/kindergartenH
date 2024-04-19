281b9fc82e939b9c9bf79f6b6f123b20bf155d02
tac <tacccccc21@gmail.com>
2024-04-19 13:43:01 +0900

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,7 +2,8 @@
 // この関数はページが読み込まれた後に実行されます
 window.onload = () => {
 
-    // スクロールリビールアニメーション
+    // スクロールリビールアニメーシ
+
     ScrollReveal().reveal('.c-fadeIn', {
       duration: 2000, // アニメーションの完了にかかる時間
       viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
@@ -31,7 +32,7 @@ window.onload = () => {
 
 
 
-
+// メニューアコーディオン
   document.addEventListener('DOMContentLoaded', function() {
     // 各メニュー項目（<li>）に対する処理
     const menuItems = document.querySelectorAll('li');
@@ -58,7 +59,7 @@ window.onload = () => {
                 // 0.5秒後に.dropMenuを非表示にするタイマーを設定
                 timer = setTimeout(function() {
                     dropMenu.style.display = 'none';
-                }, 500);
+                }, 220);
             }
         });
     });
@@ -79,3 +80,67 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+
+// モーダルウィンドウ
+let imageContainers = document.querySelectorAll('.swichImgModal');
+let modal = document.getElementById('imgModal');
+let modalImg = document.getElementById("img01");
+
+// divごとにクリックイベントを追加
+imageContainers.forEach(container => {
+  container.onclick = function(){
+    modal.style.display = "grid";
+    modalImg.src = this.children[0].src; // divの最初の子要素（画像）のsrc
+  }
+});
+
+// クローズボタンでモーダルを非表示にする
+modal.onclick = function(event) {
+  if (event.target === modal) { // クリックされた要素がモーダル自身なら閉じる
+    modal.style.display = "none";
+  }
+}
+
+
+
+// フッターアニメーション
+const fly = (target) => {
+  const startTime = performance.now()
+  
+  // 振幅と回転にランダムな値を設定
+  const amplitude = {
+    x: -5 + Math.random() * 10,  // -5 から 5 の範囲でランダム
+    y: 5 + Math.random() * 10,   // 5 から 15 の範囲でランダム
+    rotation: -3 + Math.random() * 6  // -3 から 3 の範囲でランダム
+  }
+
+  // 速度にランダムな値を設定
+  const speed = {
+    x: 0.0002 + Math.random() * 0.0006,  // 0.0002 から 0.0008 の範囲でランダム
+    y: 0.0005 + Math.random() * 0.0015   // 0.0005 から 0.002 の範囲でランダム
+  }
+
+  const tick = () => {
+    const diff = performance.now() - startTime
+    const x = amplitude.x * Math.sin(speed.x * diff)
+    const y = amplitude.y * Math.sin(speed.y * diff)
+    const rotation = amplitude.rotation * (1 + Math.sin(speed.y * diff))
+
+    target.style.transform = `rotate(${rotation}deg) translate(${x}%, ${y}%)`
+
+    requestAnimationFrame(tick)
+  }
+
+  tick()
+}
+
+// .bird クラスを持つ全ての要素を取得
+const targets = document.querySelectorAll('.bird')
+
+// 取得した全要素に対して fly 関数を適用
+targets.forEach(target => {
+  if (target) {
+    fly(target)
+  }
+})
